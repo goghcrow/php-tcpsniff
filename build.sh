1124d7a628f232719249766766ef5186809b7e34
chuxiaofeng <chuxiaofeng@youzan.com>
2017-10-29 15:35:12 +0800

    config.m4

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ phpize --clean
 phpize
 ./configure
 make -j2
+# sudo php -dextesnion=./module/tcpsniff.so tcpdump.php
 # make install
 # extenion=tcpsniff.so
-make clean
-phpize --clean
+# make clean
+# phpize --clean
